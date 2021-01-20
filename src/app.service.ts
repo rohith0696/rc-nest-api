676dc0eaccdd1c0a0f3e838191da911c84f8dd25
rohith0696 <60017598+rohith0696@users.noreply.github.com>
2021-01-19 21:14:28 -0600

    added bonus

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,7 @@ export class AppService {
   getHello(): string {
     return "Hello World from Rohith's app!";
   }
+  getAbout(): string {
+    return "Rohith Chittimalla, Software Developer"
+  }
 }
